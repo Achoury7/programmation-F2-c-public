46096d21c804b65a2fe65bf51204ec7bd15ef2ef
Achoury7 <56994048+Achoury7@users.noreply.github.com>
2020-11-18 09:19:14 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+        //Jeu de l'oie
+
+    /*  analyse du probleme : Il faudrait d'abord mettre les cas
+    particulier en initialisation puis, programmer le reste.
+    programmer aussi les cas particuliers  */
+
+
+
+void jeu_de_loie()
+    int pion = 1 ;
+    int n_de [6] = {1,2,3,4,5,6};
+    int d1 , d2 , d3 = 0;
+    int i = 1;
+    int case_arrive = 66;
+    int case_tetemort = 58;
+
+    printf("Veuillez lancer le Dé (Donnez deux valeurs)")
+    scanf("%d" & d1 + d2);
+
+    d1 + d2 == d3;
+    d3 == pion;
+
+    if pion = case_arrive{
+        printf("Le jeu est terminé %d\n");
+    }
+
+    if pion = case_tetemort{
+      pion = 1;
+        printf("Retournez a la case départ %d\n");
+
+    }
+
+
+    if pion = case_tetemort{
+    printf("Retournez a la case départ %d\n");
+
+    }
+
+   if pion%9 = 0;{
+   printf("Votre dé est doublé %d\n");
+}
+
+    if d1 + d2 = (1<d3<13);{
+        printf("Votre lancé de dé est érroné,reeseyez. %d\n")
+    }
+}
